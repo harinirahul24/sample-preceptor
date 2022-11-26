a4681e97dcb27a013abd4010570b82aba467dc68
harinirahul24 <110083281+harinirahul24@users.noreply.github.com>
2022-11-26 16:57:31 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,7 @@
 
 #!/bin/bash
-if ! [ -x "$(command -v httpd)" ]; then systemctl start httpd24 >&2;   exit 1; fi # install apache if not already installed
+if ! [ -x "$(command systemctl start httpd)" ]; then systemctl start httpd24 >&2;   exit 126; fi # install apache if not already installed
+
+
+
 
