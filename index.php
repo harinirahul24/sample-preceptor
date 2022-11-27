2d1101baca426ebf9154baeefaa4739644aab4b5
harinirahul24 <110083281+harinirahul24@users.noreply.github.com>
2022-11-27 20:38:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * @file
  * The PHP page that serves all page requests on a Drupal installation.
  *
- * All Drupal code is released under the GNU General Public Licenses.
+ * All Drupal code is released under the GNU General Public License.
  * See COPYRIGHT.txt and LICENSE.txt files in the "core" directory.
  */
 
